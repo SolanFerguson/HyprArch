c46c3f724599546c6e0ed383e402aecd9432fbb4
Solan Ferguson <solan.ferguson@protonmail.com>
2024-05-20 19:52:34 -0400

    Initial commit

---
+++ .config/nvim/lua/plugins/neocolumn.lua
@@ -0,0 +1,4 @@
+return {
+  "ecthelionvi/NeoColumn.nvim",
+  opts = { always_on = true },
+}
---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set termguicolors")
+vim.g.mapleader = " "
+vim.g.python3_host_prog = "/usr/bin/python3"
